6cbef9e1b7ceb74c72ce41cba1e32772aed35fa5
asgeir <asgeir2199@gmail.com>
2020-04-12 17:37:11 +0000

    Lagaði logo og bætti við Instructions Layout

--- app/src/main/java/is/hi/hbv601g/fingerDancer/MenuActivity.java
+++ app/src/main/java/is/hi/hbv601g/fingerDancer/MenuActivity.java
@@ -1,5 +1,6 @@
 package is.hi.hbv601g.fingerDancer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -70,7 +71,7 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     public void playGame(View v) {
-        Intent i = new Intent(this, MainActivity.class);
+        Intent i = new Intent(this, startMenuActivity.class);
         startActivity(i);
 
     }
--- app/src/main/java/is/hi/hbv601g/fingerDancer/Note.java
+++ app/src/main/java/is/hi/hbv601g/fingerDancer/Note.java
@@ -3,5 +3,4 @@ package is.hi.hbv601g.fingerDancer;
 public class Note {
     private boolean shouldPress;
     private int buttonPos;
-    private
 }
